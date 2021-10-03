311b1d550589af5c227acd9099b3913cd68f1eb1
chth2844 <chth2844@colorado.edu>
2021-10-03 01:02:52 -0600

    fulltest passes

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -19,17 +19,22 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 
 //#defines
 #define PORT_NO "9000"
 #define SOCKET_PROTOCOL 0
 #define LISTEN_BACKLOG 5
-#define BUFFER_LEN 100
+#define BUFFER_LEN 1000
 #define FILE_PATH "/var/tmp/aesdsocketdata"
 #define FILE_PERMISSIONS 0644
 
 //global variables
 int socket_fd,client_fd,fd;
+int total_write_count=0;
+int total_sent_count=0;
+int pid;
+
 
 void sig_handler(int signo)
 {
@@ -38,11 +43,7 @@ void sig_handler(int signo)
     
     syslog(LOG_DEBUG, "Caught signal, exiting\n");
     
-    //if(close(fd) < 0)
-    //{
-     // syslog(LOG_ERR,"Cannot close file descriptor\n");
-   // } 
-    
+
     if(close(socket_fd)<0)
     {
       syslog(LOG_ERR,"Cannot close server socket file descriptor\n");
@@ -53,10 +54,16 @@ void sig_handler(int signo)
       syslog(LOG_ERR,"Cannot close client socket file descriptor\n");
     }
     
-    //if(remove(FILE_PATH)<0)
-    //{
-     // syslog(LOG_ERR,"Cannot remove file\n");
-    //}
+    if(close(fd) < 0)
+    {
+      syslog(LOG_ERR,"Cannot close file descriptor\n");
+    } 
+    
+    
+    if(remove(FILE_PATH)<0)
+    {
+      syslog(LOG_ERR,"Cannot remove file\n");
+    }
     
     
     
@@ -68,19 +75,22 @@ void sig_handler(int signo)
 
 int main(int argc, char *argv[])
 {  
+
    int rc=-1;
    int status;
    char *read_buff;
    char *write_buff;
    int bytes_read = 0;
    int bytes_write = 0;
+   int isdaemon=0;
+   int reuse_addr =1;
    struct addrinfo hints;
    struct addrinfo *res; //points to result of getaddrinfo
    struct sockaddr_in client_addr;
    socklen_t client_addr_size;
    client_addr_size = sizeof(struct sockaddr_in);
    char *client_ip;
-   sigset_t signmask;
+   //sigset_t signmask;
    
    
    
@@ -106,6 +116,13 @@ int main(int argc, char *argv[])
      return rc;
    } 
    
+   if((argc == 2) && (strcmp("-d", argv[1]) == 0))
+   {
+		
+      isdaemon = 1;
+			
+    }
+	
    if((socket_fd = socket(AF_INET,SOCK_STREAM,SOCKET_PROTOCOL))<0)
    {
    
@@ -114,11 +131,13 @@ int main(int argc, char *argv[])
    
    }
    
-   if(socket_fd == -1)
-   { 
-     syslog(LOG_ERR,"Cannot obtain socket file descriptor\n");
-     return rc;
-   }
+   if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(int)) < 0) //resuse socket address 
+   {
+        syslog(LOG_ERR, "reuse addreess failed");
+        close(socket_fd);
+	closelog();
+        return rc;
+    }
    
    if((status = getaddrinfo(NULL,PORT_NO,&hints,&res)) !=0)
    {  
@@ -135,6 +154,55 @@ int main(int argc, char *argv[])
    
    freeaddrinfo(res);
    
+   if(isdaemon==1)
+    {  
+       int devnull_fd;
+       int session_id;
+       
+    	pid = fork();
+
+    	if (pid < 0 )
+    	{
+	    syslog(LOG_ERR, "fork error\n");
+	}
+
+    	if(pid > 0)
+    	{
+	    syslog(LOG_DEBUG, "daemon created with success\n");
+	    exit(0);
+    	}
+    	
+    	if(pid == 0)
+    	{
+	    session_id = setsid();
+
+            if(session_id == -1)
+	    {
+	    	syslog(LOG_ERR, "error in setting sid\n");
+		close(socket_fd);
+		closelog();
+	    	exit(EXIT_FAILURE);
+	    }
+
+            if (chdir("/") == -1)
+	    {
+            	syslog(LOG_ERR, "chdir error");
+            	closelog();
+            	close(socket_fd);
+            	return rc;
+            }
+
+    	     devnull_fd = open("/dev/null", O_RDWR);
+            dup2(devnull_fd, STDIN_FILENO);
+            dup2(devnull_fd, STDOUT_FILENO);
+            dup2(devnull_fd, STDERR_FILENO);
+
+            close(STDIN_FILENO);
+            close(STDOUT_FILENO);
+            close(STDERR_FILENO);
+    	}
+    }
+   
    
    
    if((status = listen(socket_fd,LISTEN_BACKLOG))<0)
@@ -145,24 +213,26 @@ int main(int argc, char *argv[])
     
    
     
-    if ((sigemptyset(&signmask) == -1) || (sigaddset(&signmask, SIGINT) == -1) || (sigaddset(&signmask, SIGTERM) == -1))
+    /*if ((sigemptyset(&signmask) == -1) || (sigaddset(&signmask, SIGINT) == -1) || (sigaddset(&signmask, SIGTERM) == -1))
     {
        syslog(LOG_ERR,"failed to empty signal set");
-    }
-      
+    }*/
+     
     
     while(1)
     {
-       if((fd = open(FILE_PATH, O_RDWR | O_CREAT, FILE_PERMISSIONS) == -1))
+       
+      
+     fd = open(FILE_PATH,O_RDWR | O_APPEND | O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
+     
+     
+     if(fd==-1)
      {
-     
-     
      syslog(LOG_ERR,"Cannot open file\n");
      printf("cannot open file\n");
-     return rc;
+     return 0;
      
      }
-      
       
       if((client_fd = accept(socket_fd,(struct sockaddr *)&client_addr,&client_addr_size))<0)
       {
@@ -180,15 +250,17 @@ int main(int argc, char *argv[])
      
      else{
      
-     syslog(LOG_DEBUG,"Accepted connection from %s\n", client_ip);
+     printf("Accepted connection from %s\n", client_ip);
+     
+     //syslog(LOG_DEBUG,"Accepted connection from %s\n", client_ip);
      
      }
      
-     if (sigprocmask(SIG_BLOCK, &signmask, NULL) == -1)
+     /*if (sigprocmask(SIG_BLOCK, &signmask, NULL) == -1)
      {
         syslog(LOG_ERR,"signal block failed\n");
          return rc;
-     }
+     }*/
      
      
       read_buff = (char *) malloc(sizeof(char) * BUFFER_LEN);
@@ -200,59 +272,86 @@ int main(int argc, char *argv[])
          syslog(LOG_ERR,"recv error\n");
          return rc;
         }
+        
+        else
+        {
+        
+         bytes_write = write(fd,read_buff,bytes_read);
+         
+         if(bytes_write <0)
+         {
+           syslog(LOG_ERR,"write error\n");
+           free(write_buff);
+           free(read_buff);
+           close(socket_fd);
+           close(fd);
+           return rc;
+         }
+         
+         total_write_count +=bytes_read;
+        
+        }
+          
      
      }while(strchr(read_buff,'\n') == NULL);
      
      printf("bytes_read %d\n",bytes_read);
      
-     read_buff[bytes_read]='\0';
+     read_buff[total_write_count]='\0';
          
-     if((bytes_write = write(fd,read_buff,bytes_read))<0)
-     {
-        syslog(LOG_ERR,"write error\n");
-        return rc;
-         
-     }
-         
-     printf("read_buff: %s\n",read_buff);
+    // printf("read_buff: %s\n",read_buff);
          
      lseek(fd,0,SEEK_SET);
          
-     write_buff = (char *) malloc(sizeof(char) * BUFFER_LEN);
+     write_buff = (char *) malloc(sizeof(char) *BUFFER_LEN );
          
          
-     if((bytes_read = read(fd,write_buff,BUFFER_LEN))<0)
-       {
-           syslog(LOG_ERR,"read error\n");
-           return rc;
-       }
-         
-       printf("write buffer: %s\n",write_buff);
-       printf("bytes read from file %d\n",bytes_read);
-         
-         
-       if (send(client_fd, write_buff,bytes_read, 0) < 0)
-       {
+       
+       
+       total_sent_count = 0;
+       
+       while(total_sent_count<total_write_count)
+       {   
+            lseek(fd,total_sent_count, SEEK_SET);
+            bytes_read = read(fd,write_buff,BUFFER_LEN);
+       
+            if(bytes_read == -1)
+           {
+             syslog(LOG_ERR,"read error\n");
+             return rc;
+           }
+           
+           total_sent_count+=bytes_read;
+           
+            if (send(client_fd, write_buff,bytes_read, 0) < 0)
+          {
             syslog(LOG_ERR,"send error\n");
             return rc;
-            
+          }
+           
+           
+           
        }
+       
+       
+       free(read_buff);
+       free(write_buff);
+       close(client_fd);
+       close(fd);
+         
+         
+             
+       
+    }
       
       
-       if (sigprocmask(SIG_UNBLOCK, &signmask, NULL) == -1)
+       /*if (sigprocmask(SIG_UNBLOCK, &signmask, NULL) == -1)
      {
        syslog(LOG_ERR,"signal unblock failed\n");
        return rc;
-     }
+     }*/
       
-         free(read_buff);
-         free(write_buff);
-         close(client_fd);
-         close(fd);
-         
-     }
 
-         
      closelog();
      rc =0;
      
